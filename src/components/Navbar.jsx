3a870d3bc2e3207fca5d127b24e615b2d128fa0d
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-08-30 18:43:00 +0530

    complate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,6 @@ const Navbar = () => {
             </div>
           ))}
         </div>
-
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
