d153274141c1f4025e2ebefc75ecf4f9b16f3e83
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-08-30 18:51:11 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { footerLinks } from "../constants";
 
 const Footer = () => {
   return (
@@ -22,6 +23,17 @@ const Footer = () => {
           <p className="font-semibold text-gray text-xs">
             Copyright @ 2024 Apple Inc. All rights reserved.
           </p>
+
+          <div className="flex">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}
+                {i !== footerLinks.lenght - 1 && (
+                  <span className="mx-2">|</span>
+                )}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
